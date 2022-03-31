babf5cae1a80fb525e17f8310dbd2ad5b12f1c6d
Tiemi Faustino <hayashipharma@gmail.com>
2022-03-31 15:26:45 -0300

    Redux configurado - Requisito 1 em andamento

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
-function App() {
-  return <div>Hello!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,35 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Login</h1>
+
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              data-testid="email-input"
+              type="email"
+              name="email"
+              id="email"
+            />
+          </label>
+
+          <label htmlFor="password">
+            Senha:
+            <input
+              data-testid="password-input"
+              type="password"
+              name="password"
+              id="password"
+            />
+          </label>
+
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
