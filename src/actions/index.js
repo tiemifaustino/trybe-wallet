08e657ff477bded7741894684e91e992c564e3a8
Tiemi Faustino <hayashipharma@gmail.com>
2022-03-31 19:21:22 -0300

    Requisito 4 em andamento

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,39 @@
 // Coloque aqui suas actions
 
 export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const RECEIVE_CURRENCY_SUCESS = 'RECEIVE_CURRENCY_SUCESS';
+export const RECEIVE_CURRENCY_FAILURE = 'RECEIVE_CURRENCY_FAILURE';
 
 export const saveEmail = (email) => ({
   type: SAVE_EMAIL,
   email,
 });
+
+// export const saveCurrency = () => ({
+//   type: SAVE_CURRENCY,
+//   currencies: [],
+//   expenses: [],
+// });
+
+export const receiveCurrencySucess = (currency) => ({
+  type: RECEIVE_CURRENCY_SUCESS,
+  currencies: currency,
+  expenses: [],
+});
+
+export const receiveCurrencyFailure = (error) => ({
+  type: RECEIVE_CURRENCY_FAILURE,
+  error,
+});
+
+export function fetchCurrencies() {
+  return async (dispatch) => {
+    try {
+      const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+      const data = await response.json();
+      dispatch(receiveCurrencySucess(data));
+    } catch (error) {
+      dispatch(receiveCurrencyFailure(error));
+    }
+  };
+}
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,9 +1,18 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { RECEIVE_CURRENCY_SUCESS, RECEIVE_CURRENCY_FAILURE } from '../actions';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
 
 const wallet = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case RECEIVE_CURRENCY_SUCESS:
+    return {
+      ...state,
+      currencies: [...state.currencies, action.currencies],
+    };
   default:
     return state;
   }
