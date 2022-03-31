e1c2bda6d110a931fe357847e4f5a6ce157502f4
Tiemi Faustino <hayashipharma@gmail.com>
2022-03-31 18:50:29 -0300

    Requisito 3. Crie um header para a página de carteira

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+  render() {
+    const { userEmail } = this.props;
+
+    return (
+      <div>
+        <h2>TrybeWallet</h2>
+        <p data-testid="email-field">{ userEmail }</p>
+        <div>
+          <span>Despesa total: R$</span>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">
+            {' '}
+            BRL
+          </span>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Header.propTypes = ({
+  userEmail: PropTypes.string,
+}).isRequire;
+
+export default connect(mapStateToProps)(Header);
